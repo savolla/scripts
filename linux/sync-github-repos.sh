dd4a7888f11c13e9f2337018ee438f2d0daed46b
savolla <savolla@protonmail.com>
2025-09-13 00:12:51 +0300

    auto: sync 2025.09.13 00:12

--- linux/sync-github-repos.sh
+++ linux/sync-github-repos.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
+TIMEOUT=30m
 REPO_LIST=(
     "$HOME/project/publishing/savolla.github.io" # blog
     "$HOME/project/scripts" # scripts
-    "$HOME/project/dotfiles" # dotfiles
     "$HOME/project/environ" # environ
 )
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-for repo in "${REPO_LIST[@]}"; do
-    cd "$repo" || exit
-    git add .
-    git commit -m "auto: sync"
-    git push
+while true
+do
+    for repo in "${REPO_LIST[@]}"; do
+        cd "$repo" || exit
+        git add .
+        git commit -m "auto: sync $(date "+%Y.%m.%d %H:%M")"
+        git push
+    done
+
+    # pushing dotfiles
+    dotfiles add -A
+    dotfiles commit -m "auto: sync $(date "+%Y.%m.%d %H:%M")"
+    dotfiles push
+
+    sleep "$TIMEOUT"
+    echo "[INFO] Waiting for $TIMEOUT"
 done
