c7d9d78e74dd0ddfb03ffef041e7d73acda93e59
savolla <savolla@protonmail.com>
2025-09-13 09:42:15 +0300

    auto: sync 2025.09.13 09:42

--- linux/sync-github-repos.sh
+++ linux/sync-github-repos.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -x
-
 TIMEOUT=30m
 REPO_LIST=(
     "$HOME/project/publishing/savolla.github.io"
@@ -21,17 +20,6 @@ while true; do
             echo "No changes in $repo"
         fi
     done
-
-    # # Dotfiles repo
-    # if [ -n "$(
-    #     git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" status --porcelain
-    # )" ]; then
-    #     git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" add -A
-    #     git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" commit -m "auto: sync $(date "+%Y.%m.%d %H:%M")"
-    #     git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" push || echo "Dotfiles push failed"
-    # else
-    #     echo "No changes in dotfiles"
-    # fi
 
     sleep "$TIMEOUT"
 done
